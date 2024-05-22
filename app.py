6b6fa6fa16d19f7638416e77830fdc579e051ab7
Владислав <vladsosnitscky@outlook.com>
2024-05-22 17:50:20 +0300

    Сделали шаблоны

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+#HTML - язык гипертекстовой информации (разметка)
+#CSS - каскадные таблицы стилей
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+
+app.run(debug=True)
